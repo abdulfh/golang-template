fa5e91a0c1aa7678a2e63628fefae2423f76cb36
Fatah <fatahhikami@gmail.com>
2021-05-31 22:38:43 +0700

    initialize

--- config/database.go
+++ config/database.go
@@ -11,20 +11,22 @@ import (
 // DatabaseOpen ..
 func DatabaseOpen() (*gorm.DB, error) {
 	// Local
-	dbuser :=  "newuser"
-	dbpass :=  "newuser"
-	dbname :=  "ottorecon"
-	dbaddres :=  "localhost"
+	dbuser := "newuser"
+	dbpass := "newuser"
+	dbname := "golangbe"
+	dbaddres := "localhost"
 
-	dbport :=  "5432"
-	sslmode :=  "disable"
+	dbport := "5432"
+	sslmode := "disable"
 	dbtimeout := "5"
 
 	args := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s connect_timeout=%s", dbaddres, dbport, dbuser, dbpass, dbname, sslmode, dbtimeout)
 	db, err := gorm.Open(postgres.Open(args), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
-	
+
+	// db.AutoMigrate(&models.Book{})
+
 	if err != nil {
 		fmt.Println("Error Connecting Database : ", err)
 		return db, err
--- main.go
+++ main.go
@@ -3,21 +3,29 @@ package main
 import (
 	"fmt"
 	"golangbe/config"
+	"golangbe/controllers"
+	"golangbe/repository"
+	"golangbe/services"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	_, err := config.DatabaseOpen()
+	db, _ := config.DatabaseOpen()
+
+	bookRepository := repository.InitBookRepository(db)
+	bookServices := services.InitBookServices(bookRepository)
+	bookController := controllers.InitBookController(bookServices)
 
 	router := gin.Default()
 
 	api := router.Group("/api/v1")
 	{
-		api.GET("/")
+		api.GET("/book", bookController.List)
+		api.POST("/book", bookController.Store)
 	}
 
-	err = router.Run(":8080")
+	err := router.Run(":8080")
 
 	if err != nil {
 		fmt.Println("Error while running server", err.Error())
