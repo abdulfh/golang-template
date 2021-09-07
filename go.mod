ab61b6559d68b65e940ee5446cd19f6c3d885988
Abdul Fatah <fatahhikami@gmail.com>
2021-09-07 20:03:31 +0700

    Revert "Initial Swagger"
    This reverts commit d6dd22b35af2c8f5916a0a269bfc466068459344.

--- go.mod
+++ go.mod
@@ -3,21 +3,11 @@ module golangbe
 go 1.16
 
 require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/gin-contrib/cors v1.3.1
-	github.com/gin-gonic/gin v1.7.4 // indirect
-	github.com/go-openapi/jsonreference v0.19.6 // indirect
-	github.com/go-openapi/swag v0.19.15 // indirect
-	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/swaggo/files v0.0.0-20190704085106-630677cd5c14 // indirect
-	github.com/swaggo/gin-swagger v1.3.1 // indirect
-	github.com/swaggo/swag v1.7.1 // indirect
-	golang.org/x/net v0.0.0-20210903162142-ad29c8ab022f // indirect
-	golang.org/x/sys v0.0.0-20210906170528-6f6e22806c34 // indirect
-	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/tools v0.1.5 // indirect
+	github.com/gin-gonic/gin v1.7.2 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v9 v9.29.1 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 	gorm.io/driver/postgres v1.1.0
 	gorm.io/gorm v1.21.9
 )
--- routers/router.go
+++ routers/router.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"golangbe/config"
 	"golangbe/controllers"
-	"golangbe/docs"
 	"golangbe/repository"
 	"golangbe/services"
 	"golangbe/utils/helper"
@@ -12,8 +11,6 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	ginSwagger "github.com/swaggo/gin-swagger"
-	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 type GoRouter struct {
@@ -50,15 +47,8 @@ func (goRouter *GoRouter) Routers() {
 	bookServices := services.InitBookServices(bookRepository)
 	bookController := controllers.InitBookController(bookServices)
 
-	docs.SwaggerInfo.Title = "Golang Template Swagger"
-	docs.SwaggerInfo.Description = "This is a list of sample api for Golang Template."
-	docs.SwaggerInfo.Version = "1.0"
-	docs.SwaggerInfo.Schemes = []string{"http", "https"}
-	docs.SwaggerInfo.Host = fmt.Sprintf("%s%s", helper.GetEnv("SWAGGER_HOST", "localhost"), helper.GetEnv("SERVER_PORT", ":40001"))
-
 	api := router.Group("/api/v1")
 	{
-		api.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 		api.GET("/book", bookController.List)
 		api.POST("/book", bookController.Store)
 	}
